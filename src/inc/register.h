7783d030d14b2c172ccb2fe4b1651fcf221b7b45
Jarek <orel91@o2.pl>
2019-11-21 21:09:42 +0100

    Reconfig mcu clock

---
+++ src/inc/register.h
@@ -0,0 +1,33 @@
+/*
+ * register.h
+ *
+ *  Created on: 20 lis 2019
+ *      Author: jarek
+ */
+
+#ifndef SRC_INC_REGISTER_H_
+#define SRC_INC_REGISTER_H_
+
+
+
+
+#define SET_BIT(REG, BIT)     ((REG) |= (BIT))
+
+#define CLEAR_BIT(REG, BIT)   ((REG) &= ~(BIT))
+
+#define READ_BIT(REG, BIT)    ((REG) & (BIT))
+
+#define CLEAR_REG(REG)        ((REG) = (0x0))
+
+#define WRITE_REG(REG, VAL)   ((REG) = (VAL))
+
+#define READ_REG(REG)         ((REG))
+
+#define MODIFY_REG(REG, CLEARMASK, SETMASK)  WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
+
+#define POSITION_VAL(VAL)     (__CLZ(__RBIT(VAL)))
+
+
+
+
+#endif /* SRC_INC_REGISTER_H_ */
